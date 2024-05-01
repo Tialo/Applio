0877958fa2b45cc13ad079ec3e52250e0733fe2c
Tialo <tialox@yandex.ru>
2024-05-01 13:57:50 +0300

    pre

--- app/upload_pretrain.py
+++ app/upload_pretrain.py
@@ -3,6 +3,6 @@ from utils import upload_pretrain
 upload_pretrain(
     "Titan (английский язык)",
     40000,
-    "/Users/y.korobko/Desktop/Applio/G-f040k-TITAN-Medium.pth",
-    "/Users/y.korobko/Desktop/Applio/d-f040k-TITAN-Medium.pth"
+    "G-f040k-TITAN-Medium.pth",
+    "D-f040k-TITAN-Medium.pth"
 )
